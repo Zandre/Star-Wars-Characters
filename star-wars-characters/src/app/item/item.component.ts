4b42ee006afc6979ea9c9586519fd12423066a38
Zandre Botha <botha.zandre@gmail.com>
2019-11-27 08:18:28 +0200

    Section 5 - passing data via event bindnig

--- star-wars-characters/src/app/item/item.component.ts
+++ star-wars-characters/src/app/item/item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-item',
@@ -8,10 +9,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ItemComponent implements OnInit {
 
   @Input() character;
+  @Output() sideAssigned = new EventEmitter<{name: string, side: string}>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAssign(side) {
+    this.sideAssigned.emit({name: this.character.name, side: side});
   }
 
 }
